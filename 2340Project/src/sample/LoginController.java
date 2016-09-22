785bd6eac43a220188650bb5454e32873b467620
Alok Tripathy <atripathy8@gatech.edu>
2016-09-22 16:08:18 -0400

    Added username and password authentication.

--- 2340Project/src/sample/LoginController.java
+++ 2340Project/src/sample/LoginController.java
@@ -1,11 +1,28 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 public class LoginController {
 
 	@FXML
+	private TextField userField;
+	
+	@FXML
+	private PasswordField passwordField;
+	
+	@FXML
 	public void handleOKPressed() {
-		System.out.println("herehere");
+		String user = userField.getText();
+		String passwd = passwordField.getText();
+		
+		System.out.println("user: " + user);
+		System.out.println("passwd: " + passwd);
+		if (user.equals("JWASP") && passwd.equals("/etc/passwd")) {
+			System.out.println("authenticated");
+		} else {
+			System.out.println("not authenticated");
+		}
 	}
 }
