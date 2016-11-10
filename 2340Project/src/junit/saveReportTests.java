cfba821450f7f37cabdc6920052e9d666b58052e
Sathvik Kadaveru <sathvik.kadaveru@gmail.com>
2016-11-09 19:16:09 -0500

    added saveReport test

---
+++ 2340Project/src/junit/saveReportTests.java
@@ -0,0 +1,31 @@
+package junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controller.NewReportController;
+import database.Model;
+
+/**
+ *Testing handleSave method in
+ *@author Sathvik Kadaveru
+ *
+ */
+public class saveReportTests {
+	
+	private NewReportController nrc;
+	
+	@Before
+	public void setup() {
+		nrc = new NewReportController();
+	}
+	
+	@Test
+	public void testReportAdded() {
+		nrc
+	}
+	
+
+}
